f82d75b4b4e2877fa9adceec19aae0305b6dd74a
atchariyaL <77600407+atchariyaL@users.noreply.github.com>
2021-01-18 14:54:10 +0700

    Update hello.c

--- hello.c
+++ hello.c
@@ -1,44 +1,44 @@
 #include<stdio.h>
 
 int main(void) {
+	
 	printf("Hello world\n");
-<<<<<<< HEAD
 	printf("Max value is %d\n", maxP(1,2));
 	printf("Min value is %d\n", minP(1,2));
+	
+	function_one();
+	function_two();
+	
 	return 0;
 }
 
-int minP(int num1, int num2) { return 0;}
-
 int maxP(int num1, int num2) { 
-
-=======
-	fuction_one();
-	fuction_two();
-	minP(1,2);
-	return 0;
+	int result;
+	   if (num1 < num2)
+	      result = num1;
+	   else
+	      result = num2;
+	
+	return result; 
 }
 
 int minP(int num1, int num2) { 
->>>>>>> 114ef59c3c79208b51ab6db95eb7584e5e4e9847
-   /* local variable declaration */
-   int result;
- 
-   if (num1 > num2)
-      result = num1;
-   else
-      result = num2;
+	int result;
+	   if (num1 > num2)
+	      result = num1;
+	   else
+	      result = num2;
 	
-   return result; 
+	return result; 
 }
 
 
 
-void fuction_one(){
+void function_one(){
 	printf("help sos ");
 }
 
-void fuction_two(){
+void function_two(){
 	printf(" hello branch");
 }
 
