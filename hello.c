8a1912175b78ec21d149165c1a98fbd31710be06
Kittirak Moungmingsuk <kittirak@yahoo.com>
2021-01-18 14:43:51 +0700

    add prime_print function

--- hello.c
+++ hello.c
@@ -30,3 +30,53 @@ void fuction_one(){
 void fuction_two(){
 	printf(" hello branch");
 }
+
+void print_prime(int maxi) {
+	for(int i=1;i<maxi;i++) {
+		int count=0;
+		for(int j=1;j<i;j++) {
+			if(i%j==0)
+				count++;
+		}
+		if(count==2) {
+			printf("%d\n",i)
+		}
+	}
+}
+			
+
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+B
+B
+B
+B
+B
+B
+B
+B
+B
+B
+B
+B
+			if(i%j==0	
