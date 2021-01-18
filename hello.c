d440dea9ae2b72e5622b86e0a7546fa08bbf54a2
atchariyaL <77600407+atchariyaL@users.noreply.github.com>
2021-01-18 14:31:54 +0700

    Update hello.c

--- hello.c
+++ hello.c
@@ -5,31 +5,12 @@ int main(void) {
 	return 0;
 }
 
-
-<<<<<<< HEAD
-   /* local variable declaration */
-   int result;
- 
-   if (num1 > num2)
-      result = num1;
-   else
-      result = num2;
- 
-   return result; 
-}
-
-int minP(int num1, int num2) { return 0;}
-
-=======
+   
 void fuction_one(){
 	printf("help sos ");
 }
-<<<<<<< HEAD
-
 
 void fuction_two(){
 	printf(" hello branch");
 }
-=======
->>>>>>> 26b97ce024863b2da6dad95ed59b4545d732a750
->>>>>>> 730f50b35a5b19fb8c4354d6d837236cc6c53ded
+
